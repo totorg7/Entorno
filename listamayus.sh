3e23d6f3508f50b5db7d1317c8282bd0870047c5
totorg7 <totorg7@gmail.com>
2023-08-08 10:36:21 -0300

    Agregado de documentacion al codigo

--- listamayus.sh
+++ listamayus.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
 archivo="archivo-palabras.txt"
-
+#Haemos un if, para que detecte si no hay argumentos, que imprima un mensaje y salga con error
 if [[ $# -eq 0 ]]; then
 
 	echo "Numero de argumentos invalidos"
 	exit 1
 else
+	#Si no sucede la otra condicion, entonces que busque todas las palabras en mayusculas
+	#Que las ordene, y luego hacer que no se repitan
 	grep -o -w '[[:upper:]]\+' "$archivo" | sort | uniq
 
 
