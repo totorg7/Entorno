3c5eb2ca228244df19cf8a77b76d39744403dd09
totorg7 <totorg7@gmail.com>
2023-08-08 17:33:45 -0300

    Correccion de codigo

--- listamayus.sh
+++ listamayus.sh
@@ -10,5 +10,6 @@ else
 	#Si no sucede la otra condicion, entonces que busque todas las palabras en mayusculas
 	#Que las ordene, y luego hacer que no se repitan
 	grep -o -w '[[:upper:]]\+' "$archivo" | sort | uniq
+fi
 
 
