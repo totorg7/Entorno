1e426a41eea8ba37f922ce0e171b5b191b3ee5b9
totorg7 <totorg7@gmail.com>
2023-08-08 16:45:15 -0300

    Agregado de documentacion

--- numeros.sh
+++ numeros.sh
@@ -2,6 +2,6 @@
 
 archivo="archivo-palabras.txt"
 
-
+#usamos un grep para poder identificar cuales cadenas son numeros, y luego las ordena y evita repeticiones
+#el \b lo usamos para denotar un limite de palabra, osea donde comienza y donde termina
 grep -o -E -w '\b[0-9]+\b' "$archivo" | sort-n | uniq
- 
