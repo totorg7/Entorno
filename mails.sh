244e2d7a7228d22dafa49d5c1c59448f96a92033
totorg7 <totorg7@gmail.com>
2023-08-09 11:23:33 -0300

    Modificacion de codigo

--- mails.sh
+++ mails.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-archivo="archivo-palabras.txt"
+echo "Por favor, ingrese el nombre del archivo: "
+read archivo
+
+if [[ ! -f "$archivo" ]]; then
+	echo "El archivo $archivo no existe"
+	exit 1
+fi
 
 #Usamos grep para buscar cadena de caracteres que coincidan ccon este patron
-grep -o -E -w '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b' "$arcchivo"
+grep -o -E -w '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b' "$archivo"
