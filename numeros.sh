33107941e98cf647bc036c9eb05dc7f9ec9556e6
totorg7 <totorg7@gmail.com>
2023-08-09 11:59:57 -0300

    Correccion de codigo

--- numeros.sh
+++ numeros.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-archivo="archivo-palabras.txt"
+echo "Ingrese el nombre del archivo: "
+read archivo
+
+if [[ ! -f "$archivo" ]]; then
+	echo "El archivo $archivo no existe"
+	exit 1
+fi
 
 #usamos un grep para poder identificar cuales cadenas son numeros, y luego las ordena y evita repeticiones
 #el \b lo usamos para denotar un limite de palabra, osea donde comienza y donde termina
-grep -o -E -w '\b[0-9]+\b' "$archivo" | sort-n | uniq
+grep -o -E -w '\b[0-9]+\b' "$archivo" | sort -n | uniq
